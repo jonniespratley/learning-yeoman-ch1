c64a0781c855a7c8f694362009794dc0a6874f01
Jonnie Spratley <jonniespratley@me.com>
2014-03-21 21:47:55 -0700

    updated ui

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,23 +1,38 @@
+/* global Handlebars */
+/* jshint undef: true, camelcase: false */
+'use strict';
 $(document).ready(function() {
 	var CLIENT_ID = 'ef2bd67b7dfb4bf8899999d61386d567';
-	var CLIENT_SECRET = '8c01b82fba454285a41a1666144538cf';
 	var ENDPOINT = 'https://api.instagram.com/v1/media/popular?callback=?';
-	var data, html, template = Handlebars.compile($('#media-item-tmpl').html());
+	var template = Handlebars.compile($('#media-item-tmpl').html());
 
 	function fetchMedia() {
-		var jqxhr = $.getJSON(ENDPOINT, {
+		$.getJSON(ENDPOINT, {
 			client_id : CLIENT_ID
 		}).done(function(results) {
-			$('#media').append(template(results));
+			
+			//handle when finished
+			console.log(results);
+			
+			//Append html
+			$('.marketing').append(template(results));
+			
+			//Apply tooltips
+			$('[rel="tooltip"]').tooltip();
 		}).fail(function(error) {
+			//Handle error
 			console.log('error', error);
 		});
-	};
+	}
 
+	//Fetch more media every 5 seconds
 	setInterval(function() {
-		console.log('fetch');
 		fetchMedia();
-	}, 5000);
+	}, 15000);
+	
+	//Initially fetch the recent media
 	fetchMedia();
-	console.log('\'Allo \'Allo!');
+	
+	
+	console.log('Hello from Yeoman');
 });
