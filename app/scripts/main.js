52d34edd08b1462d4f329e1977526c322b405436
Jonnie Spratley <jonniespratley@me.com>
2014-03-22 13:59:58 -0700

    added app.js

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,5 @@
 /* global Handlebars */
-/* jshint undef: true, camelcase: false */
-'use strict';
+/* jshint undef: true, camelcase: false */'use strict';
 $(document).ready(function() {
 	var CLIENT_ID = 'ef2bd67b7dfb4bf8899999d61386d567';
 	var ENDPOINT = 'https://api.instagram.com/v1/media/popular?callback=?';
@@ -10,13 +9,13 @@ $(document).ready(function() {
 		$.getJSON(ENDPOINT, {
 			client_id : CLIENT_ID
 		}).done(function(results) {
-			
+
 			//handle when finished
 			console.log(results);
-			
+
 			//Append html
 			$('.marketing').append(template(results));
-			
+
 			//Apply tooltips
 			$('[rel="tooltip"]').tooltip();
 		}).fail(function(error) {
@@ -29,10 +28,9 @@ $(document).ready(function() {
 	setInterval(function() {
 		fetchMedia();
 	}, 15000);
-	
+
 	//Initially fetch the recent media
 	fetchMedia();
-	
-	
+
 	console.log('Hello from Yeoman');
 });
