e4d6d844650ae64e935e96dba9a8a2db8b920528
Spratley, Jonathan Patrick <Jonnie.Spratley@ge.com>
2014-06-18 13:57:36 -0700

    fixed tests

--- test/spec/test.js
+++ test/spec/test.js
@@ -2,20 +2,20 @@
 'use strict';
 
 
+var ENDPOINT = 'https://api.instagram.com/v1/media/popular';
+var CLIENT_ID = 'ef2bd67b7dfb4bf8899999d61386d567';
 
-module.exports = {
-	before: function(){
-		var ENDPOINT = 'https://api.instagram.com/v1/media/popular';
-		var CLIENT_ID = 'ef2bd67b7dfb4bf8899999d61386d567';
-
-		var testApp, request, options = {
-			endpoint : null,
-			client_id : null
-		};
-		testApp = new App(options);
-	}
+var testApp, request, options = {
+	endpoint : null,
+	client_id : null
 };
+testApp = new App(options);
 
+
+
+test('App', function(){
+	ok(testApp.options !== null);
+});
 
 (function() {
 	describe('Testing WebApp', function() {
