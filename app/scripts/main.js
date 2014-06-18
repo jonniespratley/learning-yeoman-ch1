8de751ff3262be57ab39cff96b96415485b2d51c
Spratley, Jonathan Patrick <Jonnie.Spratley@ge.com>
2014-06-18 13:01:04 -0700

    removed sass and coffee

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,67 +1,73 @@
-(function() {'use strict';
-	window.App = (function() {
-		function App() {
+'use strict';
+
+window.App = (function () {
+	App.prototype.model = {
+		feature: {
+			title: 'Chapter 1',
+			image: 'https://dl.dropboxusercontent.com/u/26906414/learning_yeoman/images/yo-hbp.png',
+			body: 'a starting point for a modern html5 web application.'
+		},
+		features: []
+	};
+
+	App.prototype.tmpl = '<div class="jumbotron">\n	<h1>{{feature.title}}</h1>\n	<img src="{{feature.image}}" class="img-"/>\n	<p class="lead">\n		{{feature.body}}\n	</p>\n</div>\n{{#each features}}\n<div class="media">\n		    <img class="media-object pull-left img-thumbnail" src="{{image}}" alt="{{title}}"/>\n		    <h4>{{title}}</h4>\n		    <p>{{body}}</p>\n		  </div>\n	{{/each}}';
+
+	function App(options) {
+		var self, _ref;
+		this.options = options;
+
+		this.log('1 - initialize');
+		self = this;
+		this.render();
+		if (this.options.feature.endpoint) {
+			self.fetch();
 		}
+	}
 
+	App.prototype.render = function () {
+		var html, template;
+		this.log('4 - render');
+		$('.jumbotron').fadeOut();
+		$('.marketing').empty();
+		template = Handlebars.compile(this.tmpl);
+		html = template(this.model);
+		return $('.marketing').html(html);
+	};
 
-		App.prototype = {
-			model : null,
-			tmpl : '{{#each data}}\n						<div class="col-sm-3">\n							<a href="{{link}}" target="_blank" class="thumbnail" rel="tooltip" title="{{caption.text}}">\n								<img src="{{images.low_resolution.url}}" class="img-"/>\n							</a>\n						</div>\n					{{/each}}',
-			constructor : function(options) {
-				var self, _this = this;
-				this.options = options;
-				self = this;
-				if (this.options.interval) {
-					setInterval((function() {
-						return _this.fetch();
-					}), this.options.interval);
-				}
-				if (this.options.endpoint) {
-					self.fetch();
-				}
-				return this;
+	App.prototype.onSuccess = function (response) {
+		this.log('3 - onSuccess');
+		if (!response.data) {
+			this.model.features = response;
+		}
+		return this.render();
+	};
+
+	App.prototype.onError = function (error) {
+		return this.log(error);
+	};
+
+	App.prototype.fetch = function () {
+		var self;
+		this.log('2 - fetch');
+		self = this;
+		return $.ajax({
+			url: this.options.feature.endpoint,
+			dataType: 'jsonp',
+			success: function (results) {
+				return self.onSuccess(results);
 			},
-			render : function() {
-				var html, template;
-				console.log('render');
-				$('.jumbotron').fadeOut();
-				$('.marketing').empty();
-				template = Handlebars.compile(this.tmpl);
-				html = template(this.model);
-				return $('.marketing').append(html);
-			},
-			onSuccess : function(response) {
-				console.log('onSuccess');
-				this.model = response;
-				return this.render();
-			},
-			onError : function(error) {
-				return console.log('onError');
-			},
-			fetch : function() {
-				var self;
-				console.log('fetch');
-				self = this;
-				return $.ajax({
-					url : this.options.endpoint,
-					dataType : 'jsonp',
-					data : {
-						client_id : this.options.client_id
-					},
-					success : function(results) {
-						return self.onSuccess(results);
-					},
-					error : function(error) {
-						return self.onError(error);
-					}
-				});
+			error: function (error) {
+				return self.onError(error);
 			}
-		};
+		});
+	};
 
-		return App;
+	App.prototype.log = function (what) {
+		return typeof console !== "undefined" && console !== null ? console.log(what) : void 0;
+	};
 
-	})();
+	return App;
 
-	console.log("'Allo from CoffeeScript!");
+})();
 
-}).call(this);
+console.log("'Allo from CoffeeScript!");
--- test/spec/test.js
+++ test/spec/test.js
@@ -1,8 +1,10 @@
 /*global describe, it */
 'use strict';
-(function() {
-	describe('Testing WebApp', function() {
 
+
+
+module.exports = {
+	before: function(){
 		var ENDPOINT = 'https://api.instagram.com/v1/media/popular';
 		var CLIENT_ID = 'ef2bd67b7dfb4bf8899999d61386d567';
 
@@ -10,11 +12,20 @@
 			endpoint : null,
 			client_id : null
 		};
+		testApp = new App(options);
+	}
+};
+
+
+(function() {
+	describe('Testing WebApp', function() {
+
+
 
 		describe('App', function() {
 
 			it('should store options', function() {
-				testApp = new App(options);
+
 				expect(testApp.options).toEqual(options);
 			});
 
