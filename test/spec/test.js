3267ba48334c7be623dcd6ad474ce10cfbca05f1
Spratley, Jonathan Patrick <Jonnie.Spratley@ge.com>
2014-06-18 16:38:52 -0700

    updated specs

--- test/spec/test.js
+++ test/spec/test.js
@@ -1,61 +1,37 @@
-/*global describe, it */
+/*global describe, it, beforeEach */
 'use strict';
+var assert = chai.assert,
+	expect = chai, expect,
+	testApp = null,
+	options = {
+		el: '.container',
+		sitetitle: 'Learning Yeoman',
+		sitecopy: '2014 Copywrite',
+		version: '0.0.1',
+		feature: {
+			title: 'Chapter 1',
+			body: 'a starting point for a modern web application.',
+			image: 'http://goo.gl/kZZ6dX',
+			endpoint: 'http://jonniespratley.me:8181/api/v2/learning-yeoman-ch1/posts'
+		},
+		features: null,
+		menu: [
+			{name: 'About', route: '/about'},
+			{name: 'Contact', route: '/contact'}
+		]
+	};
 
+describe('Testing WebApp', function () {
 
-var ENDPOINT = 'https://api.instagram.com/v1/media/popular';
-var CLIENT_ID = 'ef2bd67b7dfb4bf8899999d61386d567';
+	beforeEach(function (done) {
+		testApp = new App(options);
+		done();
+	});
 
-var testApp, request, options = {
-	el: '.container',
-	sitetitle : 'Learning Yeoman',
-	sitecopy : '2014 Copywrite',
-	version: '0.0.1',
-	feature : {
-		title : 'Chapter 1',
-		body : 'a starting point for a modern web application.',
-		image : 'http://goo.gl/kZZ6dX',
-		endpoint : 'http://jonniespratley.me:8181/api/v2/learning-yeoman-ch1/posts'
-	},
-	features : null,
-	menu: [
-		{name: 'About', route: '/about'},
-		{name: 'Contact', route: '/contact'}
-	]
-};
-testApp = new App(options);
-
-
-
-
-(function() {
-	describe('Testing WebApp', function() {
-
-
-
-		describe('App', function() {
-
-			it('should store options', function() {
-
-				expect(testApp.options).toEqual(options);
-			});
-
-			it("should make JSONP request to the correct URL", function() {
-
-				spyOn($, "ajax");
-
-				//Create new app with endpoint
-				testApp = new App({
-					endpoint : ENDPOINT
-				});
-
-				//Set request
-				request = $.ajax.mostRecentCall.args[0];
-
-				//Assert
-				expect(request.dataType).toEqual('jsonp');
-				expect(request.url).toEqual(ENDPOINT);
-			});
-
+	describe('App', function () {
+		it('should store options', function (done) {
+			expect(testApp.options).to.equal(options);
+			done();
 		});
 	});
-})();
+});
