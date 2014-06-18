7a765c51377e43214b2893792c6f865d9b9299c7
Spratley, Jonathan Patrick <Jonnie.Spratley@ge.com>
2014-06-18 14:00:04 -0700

    updated runner

--- test/spec/test.js
+++ test/spec/test.js
@@ -6,8 +6,21 @@ var ENDPOINT = 'https://api.instagram.com/v1/media/popular';
 var CLIENT_ID = 'ef2bd67b7dfb4bf8899999d61386d567';
 
 var testApp, request, options = {
-	endpoint : null,
-	client_id : null
+	el: '.container',
+	sitetitle : 'Learning Yeoman',
+	sitecopy : '2014 Copywrite',
+	version: '0.0.1',
+	feature : {
+		title : 'Chapter 1',
+		body : 'a starting point for a modern web application.',
+		image : 'http://goo.gl/kZZ6dX',
+		endpoint : 'http://jonniespratley.me:8181/api/v2/learning-yeoman-ch1/posts'
+	},
+	features : null,
+	menu: [
+		{name: 'About', route: '/about'},
+		{name: 'Contact', route: '/contact'}
+	]
 };
 testApp = new App(options);
 
