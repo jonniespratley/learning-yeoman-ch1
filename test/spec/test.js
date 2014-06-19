3c5022fbf29e0cc24b40e654f29673ad18006b12
Jonnie Spratley <jonniespratley@me.com>
2014-06-18 21:55:12 -0700

    swtiing

--- test/spec/test.js
+++ test/spec/test.js
@@ -1,45 +1,35 @@
-/*global describe, it */'use strict';
-(function() {
-	describe('Testing App', function() {
-		var testApp;
-		var options = {
-			sitetitle : 'Learning Yeoman',
-			sitecopy : '(c) 2014',
-			feature : {
-				title : 'Chapter 1',
-				body : 'a starting point for a modern web application.',
-				image : 'http://goo.gl/kZZ6dX',
-				endpoint : 'http://jonniespratley.me:8181/api/v2/learning-yeoman-ch1/posts'
-			},
-			features : null,
-			menu: [
-				{name: 'About', route: '/about'},
-				{name: 'Contact', route: '/contact'}
-			]
-		};
-		beforeEach(function(){
-			testApp = new App({
-				el: '.container',
-				sitetitle : 'Learning Yeoman',
-				sitecopy : '(c) 2014',
-				feature : {
-					title : 'Chapter 1',
-					body : 'a starting point for a modern web application.',
-					image : 'http://goo.gl/kZZ6dX',
-					endpoint : 'http://jonniespratley.me:8181/api/v2/learning-yeoman-ch1/posts'
-				},
-				features : null,
-				menu: [
-					{name: 'About', route: '/about'},
-					{name: 'Contact', route: '/contact'}
-				]
-			});
-		});
+/*global describe, it, beforeEach */
+'use strict';
+var assert = chai.assert,
+	expect = chai, expect,
+	testApp = null,
+	options = {
+		el: '.container',
+		sitetitle: 'Learning Yeoman',
+		sitecopy: '2014 Copywrite',
+		version: '0.0.1',
+		feature: {
+			title: 'Chapter 1',
+			body: 'a starting point for a modern web application.',
+			image: 'http://goo.gl/kZZ6dX',
+			endpoint: 'http://jonniespratley.me:8181/api/v2/learning-yeoman-ch1/posts'
+		},
+		features: null,
+		menu: [
+			{name: 'About', route: '/about'},
+			{name: 'Contact', route: '/contact'}
+		]
+	};
 
-		it('should store options on the model', function() {
-			testApp = new App(options);
-			expect(testApp.config).toEqual(options);
-		});
+describe('Testing WebApp', function () {
 
+	beforeEach(function () {
+		testApp = new App(options);
 	});
-})();
+
+	describe('App', function () {
+		it('should store options', function () {
+			expect.expect(testApp.options, 'App.options').to.equal(options);
+		});
+	});
+});
