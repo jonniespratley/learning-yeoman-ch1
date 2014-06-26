6ac1eafef280616bc88ee0772d9bb3282daba3b0
Jonnie Spratley <jonniespratley@me.com>
2014-06-25 23:47:22 -0700

    updated spec

--- test/spec/test.js
+++ test/spec/test.js
@@ -9,8 +9,8 @@ var config = {
 	feature: {
 		title: 'Chapter 1',
 		body: 'a starting point for a modern web application.',
-		image: 'http://goo.gl/kZZ6dX',
-		endpoint: 'http://jonniespratley.me:8181/api/v2/learning-yeoman-ch1/posts'
+		image: 'http://goo.gl/kZZ6dX'
+		//endpoint: 'http://jonniespratley.me:8181/api/v2/learning-yeoman-ch1/posts'
 	},
 	features: null
 };
