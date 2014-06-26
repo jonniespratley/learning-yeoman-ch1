cdfbae519cf8d7a3b43d11ac668d6ff193597487
Jonnie Spratley <jonniespratley@me.com>
2014-06-25 20:00:11 -0700

    fixed jshint errors

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,44 +1,47 @@
-'use strict';
+/* global Handlebars */
 
-var App = {
-	init: function (config) {
-		console.log( '1 - initialize' );
-		this.config = config;
-		if (this.config.feature && this.config.feature.endpoint) {
-			this.fetch();
-		}
-		return this;
-	},
-	render: function () {
-		var html, template;
-		console.log( '4 - render' );
-		template = Handlebars.compile( $( this.config.el ).find( 'script[type="text/x-handlebars-template"]' ).html() );
-		html = template( this.config );
-		return $( this.config.el ).html( html );
-	},
-	onSuccess: function (response) {
-		console.log( '3 - onSuccess' );
-		this.config.features = response;
-		this.render();
-	},
-	onError: function (error) {
-		return this.log( error );
-	},
-	fetch: function () {
-		var self;
-		console.log( '2 - fetch' );
-		self = this;
-		return $.ajax( {
-			url: this.config.feature.endpoint,
-			dataType: 'jsonp',
-			success: function (results) {
-				return self.onSuccess( results );
-			},
-			error: function (error) {
-				return self.onError( error );
+(function (exports) {
+	'use strict';
+	exports.App = {
+		init: function (config) {
+			console.log( '1 - initialize' );
+			this.config = config;
+			if (this.config.feature && this.config.feature.endpoint) {
+				this.fetch();
 			}
-		} );
-	}
-};
+			return this;
+		},
+		render: function () {
+			var html, template;
+			console.log( '4 - render' );
+			template = Handlebars.compile( $( this.config.el ).find( 'script[type="text/x-handlebars-template"]' ).html() );
+			html = template( this.config );
+			return $( this.config.el ).html( html );
+		},
+		onSuccess: function (response) {
+			console.log( '3 - onSuccess' );
+			this.config.features = response;
+			this.render();
+		},
+		onError: function (error) {
+			return this.log( error );
+		},
+		fetch: function () {
+			var self;
+			console.log( '2 - fetch' );
+			self = this;
+			return $.ajax( {
+				url: this.config.feature.endpoint,
+				dataType: 'jsonp',
+				success: function (results) {
+					return self.onSuccess( results );
+				},
+				error: function (error) {
+					return self.onError( error );
+				}
+			} );
+		}
+	};
+})( window );
 
-console.log( "Allo, Allo!" );
+console.log( 'Allo, Allo!' );
